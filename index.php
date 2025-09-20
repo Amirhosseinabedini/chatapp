564e499642326db1abb30bdb181da2025c87fdc1
Amirhossein Abedini <abediniamirhossein1996@gmail.com>
2025-09-20 17:24:30 +0200

    Fix local development: restore original index.php and add .env file

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
 <?php
-ini_set('memory_limit', '512M');
-
-$_ENV['APP_ENV'] = 'prod';
-$_ENV['APP_SECRET'] = 'ThisIsAVerySecureSecretKeyForProduction123456789';
-$_ENV['DATABASE_URL'] = 'sqlite:///' . __DIR__ . '/var/data.db';
-
-require_once __DIR__.'/vendor/autoload.php';
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new Kernel('prod', false);
-$request = Request::createFromGlobals();
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-// If accessing root, change to simple-login
-if ($request->getPathInfo() === '/') {
-    $request = Request::create('/simple-login', $request->getMethod(), $request->request->all());
-}
-
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
